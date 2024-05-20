a9ded525d36922d4e73b65ac186472952da493f8
ngovanvu2003 <ngovanvu203@gmail.com>
2024-05-20 10:56:04 +0700

    edit spotify.js

--- spotify.js
+++ spotify.js
@@ -1,25 +1,16 @@
-const container = document.getElementsByClassName("video-ads ytp-ad-module")[0];
+observer.observe(container, { childList: true });
 
-const observer = new MutationObserver(() => {
-   // Lấy element button
-   const skipButton = document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0];
-   if (skipButton){
-      // Nhấn vào button để skip quảng cáo
-      skipButton.click();
-   }
-});
-
-observer.observe(container, {childList: true});
-
-const containerSpotify = document.getElementsByClassName("deomraqfhIAoSB3SgXpu")[0];
+const containerSpotify = document.getElementsByClassName(
+  "deomraqfhIAoSB3SgXpu"
+)[0];
 
 const spotify = new MutationObserver(() => {
-   // Lấy element button
-   const skipButton = document.getElementsByClassName("mnipjT4SLDMgwiDCEnRC")[0];
-   if (skipButton){
-      // Nhấn vào button để skip quảng cáo
-      skipButton.click();
-   }
+  // Lấy element button
+  const skipButton = document.getElementsByClassName("mnipjT4SLDMgwiDCEnRC")[0];
+  if (skipButton) {
+    // Nhấn vào button để skip quảng cáo
+    skipButton.click();
+  }
 });
 
-spotify.observe(containerSpotify, {childList: true});
+spotify.observe(containerSpotify, { childList: true });
